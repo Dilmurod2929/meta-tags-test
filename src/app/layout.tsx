bc457e38230a1c4978ad6e8bb3c69de1fb0dd1c8
Dilmurod <dilmurod8829@gmail.com>
2024-10-02 12:14:49 +0500

    test-7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     url: "https://meta-tags-test.vercel.app",
     images: [
       {
-        url: "https://meta-tags-test.vercel.app/test.jpg",
+        url: "https://meta-tags-test.vercel.app//public/test.jpg",
         width: 1200,
         height: 630,
         alt: "logo",
